c28a2acd08c6e9094c040cc528d51b95373e0cb2
ashishdeopandey <ashu1234mani@gmail.com>
2024-06-24 12:20:12 +0530

    gitt

--- src/components/Head/Head.jsx
+++ src/components/Head/Head.jsx
@@ -9,7 +9,7 @@ export const Head = () => {
         <section className={styles.container}>
             <div className={styles.content}>
                 <h1 className={styles.title}>Hi! I'm Ashish Pandey</h1>
-                <p className={styles.discription}>I have 4 years of experience as a full-stack developer, working with React and Java. I'm very passionate and dedicated </p>
+                <p className={styles.discription}>I have 4 years of experience as a full-stack developer, working with React and Java</p>
                 <a href= {resume} download="Resume" className={styles.cBtn}>Download CV <i class="uil uil-file-download-alt"></i></a>
             </div>
             <img src={getImageUrl("hero/Ashish Deo Pandey.jpeg")} alt="head image" className={styles.headImg} />
